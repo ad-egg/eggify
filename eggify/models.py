f7bc5507ab974320368673d14b9ebff3d75ab8e2
Athena Deng <539@holbertonschool.com>
2019-06-07 19:50:24 +0000

    rearranged lines for better organization

--- eggify/models.py
+++ eggify/models.py
@@ -11,17 +11,21 @@ from django.utils import timezone
 class Eggnt(models.Model):
     """a class Eggnt"""
 
-    class Meta:
-        ordering = ('updated_at', )
-
-    def __str__(self):
-        """returns the words"""
-        return self.words
-
+    # fields/columns in database
     id = models.CharField(max_length=50, default=str(uuid.uuid4()), primary_key=True, editable=False)
     updated_at = models.DateTimeField('updated at', default=timezone.now(), editable=False)
     words = models.TextField(null=False, help_text='Enter text to be eggified!')
 #    user_num = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+
+    # metadata
+    class Meta:
+        ordering = ['-updated_at']
+
+    # methods
+    def __str__(self):
+        """returns the words"""
+        return self.words
+
 
 class User(models.Model):
     """a class User"""
