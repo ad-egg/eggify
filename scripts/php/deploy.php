dcfa445b257ed1d0301ae134f56435fac1b75040
root <root@ip-172-31-4-38.us-west-1.compute.internal>
2019-06-13 22:10:23 +0000

    Updating compose yml and deploy script with fixes

--- scripts/php/deploy.php
+++ scripts/php/deploy.php
@@ -1,11 +1,11 @@
 <?php
 
-  echo exec('git clone https://github.com/ad-egg/eggify.git');
-  echo exec('sudo minikube start 2>&1');
-  echo exec('sudo kubectl run eggify --image=sazad44/egg:v1 --port=8080 2>&1');
-  echo exec('sudo kubectl proxy');
-  echo exec('export POD_NAME=sudo kubectl get pods | tail -1 | cut -d " " -f 1');
-  /*echo exec('cd eggify && sudo kubectl exec $POD_NAME gunicorn --bind 0.0.0.0:8030 egg_clusters.wsgi');*/
+  echo exec('sudo git clone https://github.com/ad-egg/eggify.git');
+  echo exec('sudo rm -rf eggify/mysql/* /tmp/* 2>&1');
+  echo exec('sudo docker system prune -f 2>&1');
+  chdir('/var/www/html/eggify');
+  echo exec('sudo kompose convert');
+  echo exec('sudo kompose up');
   echo('EGGIFY DEPLOYED');
 
 ?>
